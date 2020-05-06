d7c5c0179b7dfa6c7c7240f73c0a181b199b27d0
MudHistoricalSociety <mudhistoricalsociety@gmail.com>
2020-05-06 16:52:50 -0700

    TinyMUX 2.6

---
+++ src/_build.h
@@ -0,0 +1,21 @@
+// _build.h
+//
+// $Id: _build.h,v 1.55 2006/02/03 07:45:19 sdennis Exp $
+//
+#ifndef MUX_BUILD_NUM
+extern char szBuildNum[];
+#define MUX_BUILD_NUM szBuildNum
+#endif // MUX_BUILD_NUM
+
+#ifndef MUX_BUILD_DATE
+extern char szBuildDate[];
+#define MUX_BUILD_DATE szBuildDate
+#endif // MUX_BUILD_DATE
+
+#define MUX_VERSION       "2.6.0.1"          // Version number
+#define MUX_RELEASE_DATE  "2006-FEB-02"      // Source release date
+
+// Define if this release is qualified as ALPHA or BETA.
+//
+#define ALPHA
+//#define BETA
